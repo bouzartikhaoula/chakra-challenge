f4bd6ef61f3cbe1691a46afcf5a952217f5ef80d
khaoula <bouzartikhaoula@gmail.com>
2024-08-09 15:58:30 +0200

    new

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,11 +2,13 @@ import { Box ,Text, Heading } from '@chakra-ui/react'
 import Pricing from './Pricing'
 const Header = () => {
   return (
-    <Box as='section' color="#F7FAFC" bg="#6B46C1" pt="90px" pb="198px" px="32" textAlign='center'>
+    <>
+    <Box as='section' color="#F7FAFC" bg="#6B46C1"  pt="90px" pb="130px" px="32px" textAlign='center'>
     <Heading fontWeight="800" fontSize='48px' >Simple pricing for your business</Heading>
    <Text fontWeight="500" fontSize="24px" pb="16" >Lorem ipsum dolor sit amet consectetur adipisicing elit.</Text>
-   <Pricing/>
-    </Box>
+ 
+    </Box>  <Pricing/>
+    </>
   )
 }
 
--- src/component/Pricing.jsx
+++ src/component/Pricing.jsx
@@ -1,18 +1,18 @@
-import { Box, Heading, Text, Button, Flex } from "@chakra-ui/react";
+import { CalendarIcon, CheckIcon } from "@chakra-ui/icons";
+import { Box, Heading, List, ListIcon, Text, Button, Flex, HStack, Icon, ListItem, Center, Stack } from "@chakra-ui/react";
 import {
-  List,
-  ListItem,
-  ListIcon,
+
   OrderedList,
   UnorderedList,
 } from '@chakra-ui/react'
 import React from "react";
 
+
 const Pricing = () => {
   return (
-    <>
-      <Flex>
-        <Box>
+    <Box  w="60%" m="auto"  mt="-150px" >
+      <Flex   bg='white' rounded='md'>
+        <Box textAlign='center'  p="15px" bg='#F0EAFB' rounded='md' >
           <Text fontSize="24" fontWeight="800">
             Premium PRO
           </Text>
@@ -32,29 +32,29 @@ const Pricing = () => {
             Get Started
           </Button>
         </Box>
-        <Box>
-          <Text> Access Thes features when you get this Pricing </Text>
-          <List spacing={3}>
+        <Box m="15px">
+          <Text noOfLines={2}> Access Thes features when you get this Pricing  you get this Pricing</Text>
+          <List  mt="20px "spacing={3}>
             <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
+              <ListIcon as={CheckIcon} color="#6B46C1" rounded='md' bg={"white"} />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit
             </ListItem>
             <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
+              <ListIcon  as={CheckIcon} color="#6B46C1" rounded='md' bg={"white"} />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit
             </ListItem>
             <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
+              <ListIcon as={CheckIcon} color="#6B46C1" rounded='md' bg={"white"} />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit
             </ListItem>
             <ListItem>
-              <ListIcon as={MdCheckCircle} color="green.500" />
+              <ListIcon as={CheckIcon} color="#6B46C1" rounded='md' bg={"white"} />
               Lorem ipsum dolor sit amet, consectetur adipisicing elit
             </ListItem>
           </List>
         </Box>
       </Flex>
-    </>
+    </Box>
   );
 };
 
